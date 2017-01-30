81ada59370f1d9a4d367f95cb57cb4818e47f217
Andrey Belyaev <svetomech@gmail.com>
2017-01-30 16:30:00 +0300

    Basic functionality working

--- RearGate/Program.cs
+++ RearGate/Program.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
 
 namespace RearGate
 {
     class Program
     {
-        static void Main(string[] args)
+        const string _authorizationKey = "RearVisitor.exe";
+
+        static void Main()
         {
             var guard = GateGuard.Instance;
             var visitor = guard.Visitor;
@@ -15,20 +19,31 @@ namespace RearGate
             {
                 guard.DoDuty();
 
-                switch (visitor.State)
+                Console.WriteLine(String.Join(" ", visitor.Name, visitor.State));
+
+                if (VisitorState.Entered == visitor.State)
                 {
-                    case VisitorState.Entered:
-                        Console.WriteLine($"{visitor.Name} just entered!");
-                        break;
-
-                    case VisitorState.Left:
-                        Console.WriteLine($"{visitor.Name} left us.");
-                        break;
-
-                    default:
-                        guard.GoToBed();
-                        break;
+                    Authorize(ref visitor);
                 }
+            }
+        }
+
+        static void Authorize(ref GateVisitor visitor)
+        {
+            string keyLocation = Path.Combine(visitor.Name, _authorizationKey);
+
+            if (!File.Exists(keyLocation))
+            {
+                return;
+            }
+
+            try
+            {
+                Process.Start(keyLocation);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
             }
         }
     }
